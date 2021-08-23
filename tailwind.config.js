f520f779616d877a9c4fa1ec0c88a552ad9e404f
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-08-23 19:08:38 +0800

    Added a section for Home

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,28 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'primary': '#FBFBFB',
+      'secondary': '#FFF1C1',
+      'tertiary': '#78B7BB',
+      'highlight': '#808B97'
+     }),
     extend: {
       fontFamily: {
         primary: ['Ubuntu', 'sans-serif'],
         secondary: ['Prompt', 'sans-serif'],
       },
+      borderRadius: {
+        '4xl': '4.5rem',
+        '5xl': '5rem',
+      },
+      textColor: {
+        'primary': '#FBFBFB',
+        'secondary': '#FFF1C1',
+        'tertiary': '#78B7BB',
+        'highlight': '#808B97',
+      }
     },
   },
   variants: {
