e5ece918912a941e0794fa20e0b6db4282bd23ef
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-08-23 10:41:09 +0800

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ['Ubuntu', 'sans-serif'],
+        secondary: ['Prompt', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
