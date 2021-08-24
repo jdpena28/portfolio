bf94aa600020c43faa9ad7c752c4c01abf2038a6
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-08-24 10:46:18 +0800

    added active nav-link animation at navbar

--- public/index.js
+++ public/index.js
@@ -5,3 +5,26 @@ const menu = document.querySelector(".mobile-menu");
 btn.addEventListener("click", () => {
   menu.classList.toggle("hidden");
 });
+
+// navbar active underline function
+var items = document.getElementsByClassName('nav-link');
+var activeClassName = 'active';
+
+function unselectItems() {
+  for (var i = 0; i < items.length; i++) {  
+    items[i].classList.remove('border-b-4' ,'border-blue-500');
+  }
+}
+
+function selectItem(item) {
+  unselectItems();
+  item.classList.add('border-b-4' ,'border-blue-500');
+}
+
+function onItemClick(event) {
+  selectItem(event.target);
+}
+
+for (var i = 0; i < items.length; i++) {  
+  items[i].addEventListener('click', onItemClick);
+}
