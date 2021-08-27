311bb4bdbc4f569162027a24dd19299e934bdc9a
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-08-27 22:49:00 +0800

    Created mobile device view

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,18 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      'sm': {'max':'480px'},
+      //custom for mobile devices
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     backgroundColor: theme => ({
       ...theme('colors'),
       'primary': '#FBFBFB',
