5b2c3945e7f83acd577539e90c81e76e653775a1
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-10-26 09:17:09 +0800

    Add Stacks component for about section

--- components/Stacks.tsx
+++ components/Stacks.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Image from 'next/image'
 
-const Stacks = () => {
+const Stacks:React.FC<Stacks> = ({stacksTitle,stacksUrl}) => {
     return (
         <div>
-            
+            <h4><span className= "font-primary font-normal text-purple-600">.</span>
+            {stacksTitle}
+            <span className='font-primary font-normal text-purple-600'>&#40;&#41;</span></h4>
+            <div className='flex gap-x-2'>
+                {stacksUrl.map(e => {
+                    return (
+                        <Image src = {e} height = {32} width = {32}/>
+                    )
+                })}
+            </div>
         </div>
     )
 }
--- types/props.d.ts
+++ types/props.d.ts
@@ -2,3 +2,7 @@ interface NavLinks {
     path: string
     title: string
 }
+interface Stacks {
+    stacksTitle: string
+    stacksUrl: string[]
+}
