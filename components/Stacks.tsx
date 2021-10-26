fe51ea9af0fd307b0bfd5acc7c0bcc8915adfa37
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-10-26 22:10:57 +0800

    Change color of some svg for compliment

--- components/Stacks.tsx
+++ components/Stacks.tsx
@@ -7,10 +7,10 @@ const Stacks:React.FC<Stacks> = ({stacksTitle,stacksUrl,flex}) => {
             <h4 className = 'text-3xl font-secondary font-normal'><span className= "font-primary font-normal text-purple-600">.</span>
             {stacksTitle}
             <span className='font-primary font-normal text-purple-600'>&#40;&#41;</span></h4>
-            <div className='flex gap-x-2 mt-4'>
+            <div className='flex gap-x-5 mt-4'>
                 {stacksUrl.map(e => {
                     return (
-                        <Image src = {e} height = {69} width = {69}/>
+                        <Image  src = {e} height = {69} width = {69}/>
                     )
                 })}
             </div>
