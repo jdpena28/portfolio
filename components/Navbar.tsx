fe0eb32da77cc535b7ba17dbf37043983d285ea5
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-10-27 08:31:12 +0800

    Codes linted

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,8 +2,7 @@
 import React from 'react'
 import Link from 'next/link'
 
-
-const NavLinks:React.FC<NavLinks> = ({path,title}) => {
+const NavLinks: React.FC<NavLinks> = ({ path, title }) => {
 	return (
 		<Link href = {path}>
 			<a>{title}</a>
@@ -11,18 +10,17 @@ const NavLinks:React.FC<NavLinks> = ({path,title}) => {
 	)
 }
 
-
-const Navbar:React.FC = () => {
+const Navbar: React.FC = () => {
 	return (
 		<nav>
 			<div className='flex justify-between mx-12 text-lg p-5'>
 				<p>jdpena</p>
-          <div className='flex gap-x-3 font-normal'>
-						<NavLinks path={'/#home'} title={'Home'} /> /
-						<NavLinks path={'/#about'} title={'About'} /> /
-						<NavLinks path={'/#project'} title={'Projects'} /> /
-						<NavLinks path={'/#contacts'} title={'Contacts'} />
-          </div>
+				<div className='flex gap-x-3 font-normal'>
+					<NavLinks path={'/#home'} title={'Home'} /> /
+					<NavLinks path={'/#about'} title={'About'} /> /
+					<NavLinks path={'/#project'} title={'Projects'} /> /
+					<NavLinks path={'/#contacts'} title={'Contacts'} />
+				</div>
 			</div>
 		</nav>
 	)
