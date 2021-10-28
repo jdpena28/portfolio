b222dfee29f0fe859991ffe536beced3c4466d45
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-10-28 17:41:42 +0800

    Fix color of input fields

--- components/Sections/Contact.tsx
+++ components/Sections/Contact.tsx
@@ -1,17 +1,20 @@
 /** @format */
 import Image from 'next/image'
-import { TextField, Button,styled } from '@mui/material'
+import { TextField, Button,styled, OutlinedInputProps, TextFieldProps } from '@mui/material'
 
 const ValidationTextField = styled(TextField)({
 	'& .MuiOutlinedInput-root': {
 	  '& fieldset': {
-		borderColor: 'white',
+		borderColor: '#f5f5f5',
 	  },
 	  '&.Mui-focused fieldset': {
-		borderColor: 'white',
+		borderColor: '#f8f8ff',
 	  },
 	  '&:hover fieldset': {
-		borderColor: 'white',
+		borderColor: '#f8f8ff',
+	  },
+	  '& input': {
+		  color: '#f8f8ff'
 	  },
 	},
 	'& input:valid:focus + fieldset': {
@@ -19,21 +22,43 @@ const ValidationTextField = styled(TextField)({
 	  padding: '4px !important', // override inline-style
 	},
 	' & label.Mui-focused': { 
-	  color: 'white'
+	  color: '#f8f8ff'
 	},
 	' & label': {
-	  color: 'white'
+	  color: '#f8f8ff'
 	},
   });
+
+  const Multiline = styled((props: TextFieldProps) => (
+	<TextField
+	  InputProps={{ disableUnderline: true } as Partial<OutlinedInputProps>}
+	  {...props}
+	/>
+  ))(({ theme }) => ({
+	'& .MuiFilledInput-root': {
+	  border: '1px solid white',
+	  overflow: 'hidden',
+	  borderRadius: 4,
+	  backgroundColor: '#f8f8ff',
+	  transition: theme.transitions.create([
+		'border-color',
+		'background-color',
+		'box-shadow',
+	  ]),
+	  '&.Mui-focused': {
+		borderColor: '#f5f5f5'
+	  },
+	},
+  }));
 
 const Contact = () => {
 	return (
 		<section className='w-full h-screen relative' id='contacts'>
-			<h3 className='font-secondary opacity-[3%] text-9xl font-bold tracking-widest absolute top-[15%] left-11 z-[1] cursor-default'>
+			<h3 className='font-secondary opacity-[3%] text-9xl font-bold tracking-widest absolute top-0 left-11 z-[1] cursor-default'>
 				contact
 			</h3>
-			<div className='container h-full mx-auto flex items-center justify-center'>
-				<div className='bg-gray-500 opacity-80 w-[75%] h-[69%] z-30 rounded-3xl relative'>
+			<div className='container mt-14 h-full mx-auto flex items-center justify-center'>
+				<div className='bg-gray-500 opacity-90 w-[75%] h-[69%] z-30 rounded-3xl relative'>
 					<div className='absolute -bottom-3 right-0 w-80 h-80 flipImg'>
 						<Image src={'/contact-us.svg'} layout='fill' />
 					</div>
@@ -45,7 +70,7 @@ const Contact = () => {
 							required
 						/>
 						<ValidationTextField className='w-72' label='Name' variant='outlined' />
-						<TextField
+						<Multiline
 							className='w-72'
 							label='Message'
 							multiline
@@ -54,7 +79,7 @@ const Contact = () => {
 							required
 						/>
 						<Button className = 'w-72' variant='contained' size='medium'>
-							Submit
+							Send
 						</Button>
 					</form>
 				</div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,17 +6,15 @@ import About from '../components/Sections/About'
 import Contact from '../components/Sections/Contact'
 import Home from '../components/Sections/Home'
 import Project from '../components/Sections/Project'
-import Test from '../components/Sections/Test'
 
 const App: React.FC = () => {
 	return (
 		<div className='font-primary text-gray-200 bg-gray-900 mx-12 space-y-11'>
-				<Navbar />
-				<Home />
-				<About />
-				<Project/>
-				<Contact />
-				<Test />
+			<Navbar />
+			<Home />
+			<About />
+			<Project />
+			<Contact />
 		</div>
 	)
 }
