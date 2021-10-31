b0f4c9bde1d3c58942ff17705b4cc329aa9617c5
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-10-31 10:29:02 +0800

    Add redirect for my social media site

--- next.config.js
+++ next.config.js
@@ -2,3 +2,37 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+
+/* Social Media Redirects */
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/facebook',
+        destination: 'https://www.facebook.com/Jang.Henrich/',
+        permanent: false,
+        basePath: false,
+      },
+      {
+        source: '/linkedin',
+        destination: 'https://www.linkedin.com/in/johnhenrichdelapena/',
+        permanent: false,
+        basePath: false,
+      },
+      {
+        source: '/instagram',
+        destination: 'https://www.instagram.com/jdpena28/',
+        permanent: false,
+        basePath: false,
+      },
+      {
+        source: '/github',
+        destination: 'https://github.com/jdpena28',
+        permanent: false,
+        basePath: false,
+      }
+    ]
+  },
+}
