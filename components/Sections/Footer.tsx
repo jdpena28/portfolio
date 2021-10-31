721d05bd603722e31c95460c6791253af23ace77
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-10-31 22:11:38 +0800

    Feature: mobile responsive for footer

--- components/Sections/Footer.tsx
+++ components/Sections/Footer.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import {FaFacebookF,FaLinkedinIn,FaInstagram,FaGithub} from 'react-icons/fa'
-
+import {FaFacebookF,FaLinkedinIn,FaInstagram,FaGithub,FaRegCopyright} from 'react-icons/fa'
+import {MdCopyright} from 'react-icons/md'
 const Footer = () => {
     return (
-        <footer className = 'flex justify-between items-center p-4 border-t-2 border-white'>
+        <footer className = 'flex sm:flex-col-reverse justify-between items-center p-4 border-t-2 border-white sm:text-center sm:gap-y-3 font-light'>
             <div>
-                <p className='text-base'>&#9400; John Henrich Dela Peña</p>
+                <p className='text-base inline'><span><FaRegCopyright size={17} className='inline -mt-1'/></span> 2021 John Henrich Dela Peña</p>
                 <p>All Rights Reserved.</p>
             </div>
-            <div className="flex gap-x-2">
+            <div className="flex gap-x-2 sm:gap-x-4">
                 <a href="#" target = '_blank'><FaFacebookF size ={22}/></a>
                 <a href="#" target = '_blank'><FaLinkedinIn size ={22}/></a>
                 <a href="#" target = '_blank'><FaInstagram size ={22}/></a>
