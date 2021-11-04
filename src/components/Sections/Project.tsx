0a3c884702f1ab51a5080eec3089653fd8781c32
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-11-04 22:06:58 +0800

    Add keys for array elements

--- src/components/Sections/Project.tsx
+++ src/components/Sections/Project.tsx
@@ -5,6 +5,7 @@ import { project } from '../data'
 import EachProject from '../EachProject'
 
 const Project = () => {
+	let num = 11
 	return (
 		<section className='w-full min-h-screen pb-6 relative' id='project'>
 			<h3 className='font-secondary opacity-[3%] text-9xl sm:text-7xl font-bold tracking-widest absolute top-2 left-1 z-[1] cursor-default'>
@@ -22,6 +23,7 @@ const Project = () => {
                             num={e.num}
 							titleClass={e.titleClass}
 							descClass={e.descClass}
+							key = {num+=1}
 						/>
 					)
 				})}
