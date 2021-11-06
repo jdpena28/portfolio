e448ebc66fc2b68005af1ea808c90a6d089cb5a2
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-11-06 16:07:58 +0800

    Feature: Add animation onHover

--- src/components/EachProject.tsx
+++ src/components/EachProject.tsx
@@ -3,16 +3,44 @@ import React,{useState} from 'react'
 import {BiLinkExternal} from 'react-icons/bi'
 import {VscGithub} from 'react-icons/vsc'
 import Image from 'next/image'
+import {AnimatePresence, motion} from 'framer-motion'
 
 const EachProject:React.FC<EachProject> = ({flex,title,desc,src,color,num,titleClass,descClass,hoverClass,hoverDesc,github,link}) => {
 	const [imgHover,setImgHover] = useState<boolean>(false)
+
+	const variants = {
+		hidden: {
+				scale: 0
+			},
+		visible: {
+			scale:1,
+			transition: {
+				duration: .3,
+				delay: .1,
+				ease: 'easeInOut',
+			}
+		},
+		exit: {
+			scale:0,
+		}
+	}
+
 	return (
 		<div>
 			<div className = {`relative sm:block flex items-center gap-x-6 sm:space-y-3 ${flex}`}>
-				<div className="relative z-20 h-full w-[full " onMouseEnter={()=>setImgHover(true)} onMouseLeave={()=>setImgHover(false)}>
-					<Image src={src} width={600} height={315}/>					
+				<div className="relative z-20 h-full w-[full " 
+				onMouseEnter={()=>setImgHover(true)} onMouseLeave={()=>setImgHover(false)} 
+				onClick ={()=>imgHover?setImgHover(false):setImgHover(true)}>
+					<Image src={src} width={600} height={315}/>		
+					<AnimatePresence>			
 					{imgHover && 
-					<div className = 'absolute top-0 w-full h-full bg-gray-700 bg-opacity-60'>
+					<motion.div 
+						initial='hidden'
+						animate ='visible'
+						variants={variants}
+						exit ='exit'
+						key = {link}
+						className = 'absolute top-0 w-full h-full bg-gray-700 bg-opacity-60'>
 						<div className={`${hoverClass} w-full h-full flex items-end justify-end sm:justify-between p-2  gap-x-7 sm:gap-x-0`}>
 							<p>{hoverDesc}</p>
 							<div className='flex gap-x-1'>
@@ -20,8 +48,8 @@ const EachProject:React.FC<EachProject> = ({flex,title,desc,src,color,num,titleC
 								<a href={link}><BiLinkExternal size={26}/></a>
 							</div>
 						</div>
-					</div>
-					}
+					</motion.div>}
+					</AnimatePresence>
 				</div>
                 <h4 className={`absolute sm:static z-30 font-secondary font-semibold text-5xl sm:text-3xl ${titleClass}`}>{title}</h4>
                 <p className={`sm:hidden absolute -bottom-3 z-20 underline text-8xl  font-secondary font-semibold ${color}`}>{num}</p>
