178fcb2a861aec38ba925dd27fe8f0403e1fb4d0
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-11-07 08:53:39 +0800

    Function: onClick go to dedicated router link

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import Link from 'next/link'
 type btnProps = {
     btnClass: string|string[]
     text: string
+    btnPath: string
 }
 
-const Button:React.FC<btnProps> = ({btnClass,text}) => {
+const Button:React.FC<btnProps> = ({btnClass,text,btnPath}) => {
     return (
         <div>
-            <a href="#">
+            <Link href={btnPath}>
                 <button className = {`${btnClass} w-36 h-14 rounded-md text-lg font-semibold text-white`}>{text}</button>
-            </a>
+            </Link>
         </div>
     )
 }
