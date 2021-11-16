479546d03b055bcd9b345150066ed965b786b7f4
jdpena28 <johnhenrich1128@gmail.com>
2021-11-16 10:18:12 +0800

    Tweak: adjust size of contact for mobile

--- src/components/Sections/Contact.tsx
+++ src/components/Sections/Contact.tsx
@@ -1,6 +1,6 @@
 /** @format */
 import Image from 'next/image'
-import React, {useRef,useState} from 'react'
+import React from 'react'
 import {Parallax} from 'react-scroll-parallax'
 
 // React-Icons
@@ -25,31 +25,31 @@ const Contact = () => {
 			<div 
 			data-aos='fade-up'
 			className='container mt-14 h-full mx-auto flex items-center justify-center'>
-				<div className='bg-gray-500 opacity-90 w-[75%] sm:w-full h-[69%] sm:h-full z-30 rounded-3xl relative flex sm:flex-col sm:justify-evenly sm:items-center justify-around'>
+				<div className='bg-gray-500 opacity-90 w-[75%] sm:w-full h-[69%] sm:h-[80%] z-30 rounded-3xl relative flex sm:flex-col sm:justify-evenly sm:items-center justify-around'>
 					<div className='sm:hidden font-light absolute -bottom-3 right-0 w-80 h-80 flipImg'>
 						<Image src={'/contact-us.svg'} layout='fill' />
 					</div>
 					<form className='flex flex-col gap-y-1 my-auto sm:my-0 font-primary'>
 						<div className='flex flex-col gap-y-1'>
-							<label htmlFor="email" className='font-medium text-lg ml-1'>Email*</label>
+							<label htmlFor="email" className='font-medium text-sm ml-1'>Email*</label>
 							<input 
-							className='w-72 sm:w-96 h-12 pl-2 font-light outline-none text-white placeholder-gray-300 rounded-md bg-transparent border-[1px] border-white'
+							className='w-72 h-10 pl-2 font-light text-sm outline-none text-white placeholder-gray-300 rounded-md bg-transparent border-[1px] border-white'
 							type="text" id='email' required placeholder='johndoe@email.com' />
 						</div>
 						<div className='flex flex-col gap-y-1'>
-							<label htmlFor="name" className='font-medium text-lg ml-1'>Name</label>
+							<label htmlFor="name" className='font-medium text-sm ml-1'>Name</label>
 							<input 
-							className='w-72 sm:w-96  h-12 pl-2 font-light outline-none text-white placeholder-gray-300 rounded-md bg-transparent border-[1px] border-white '
+							className='w-72  h-12 pl-2 font-light text-sm outline-none text-white placeholder-gray-300 rounded-md bg-transparent border-[1px] border-white '
 							type="text" id='name' placeholder='John Doe ' />
 						</div>
 						<div className='flex flex-col gap-y-1'>
-							<label htmlFor="message" className='font-medium text-lg ml-1'>Message*</label>
-							<textarea className='text-base text-black placeholder-gray-600 outline-none rounded-md p-1 text-size' placeholder='Your Message' name="message" id="message" required cols={25} rows={8}></textarea>
+							<label htmlFor="message" className='font-medium text-sm ml-1'>Message*</label>
+							<textarea className='text-black placeholder-gray-600 outline-none rounded-md p-1 text-size' placeholder='Your Message' name="message" id="message" required cols={25} rows={8}></textarea>
 						</div>
-						<button type="button" onClick={notify} className='w-72 sm:w-96  bg-blue-500 text-white h-9 font-medium rounded-sm mt-3 transition-colors active:bg-blue-600 '>Submit</button>
+						<button type="button" onClick={notify} className='w-72  bg-blue-500 text-white h-9 font-medium rounded-sm mt-3 transition-colors active:bg-blue-600 '>Submit</button>
 						<ToastContainer
 						position="bottom-center"
-						toastClassName={'sm:w-[75%] mx-auto'}
+						toastClassName={'sm:w-[90%] mx-auto'}
 						autoClose={2000}
 						closeOnClick
 						pauseOnFocusLoss
@@ -57,7 +57,7 @@ const Contact = () => {
 						pauseOnHover
 						/>
 					</form> 
-					<div className='mt-6 font-normal'>
+					<div className='mt-6 sm:mt-0 font-normal'>
 						<h4 className='text-2xl font-medium mb-2'>Interested in Working?</h4>
 						<GrMail size={18} className = 'inline ml-3'/><p className='inline text-sm'>&emsp;johnhenrich1128@gmail.com</p><br/>
 						<RiContactsBookFill size={18} className='inline ml-3'/><p className='inline text-sm'>&emsp;+639468083171</p><br/>
