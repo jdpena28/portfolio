908863217d76f9bd17b57d3eb27cd9dd4cf43706
jdpena28 <johnhenrich1128@gmail.com>
2021-11-19 10:39:40 +0800

    Tweak: use pointer curso

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ const NavLinks: React.FC<NavLinks> = ({ path, title, onClick,delay}) => {
 			initial = {{y: -50}}
 			animate={{y:0}}
 			transition={{type:'spring', duration:.8 ,delay:delay, bounce:.5}}
-			className = 'hover:bg-gray-600 bg-opacity-10 rounded-lg p-1' 
+			className = 'hover:bg-gray-600 bg-opacity-10 rounded-lg p-1 cursor-pointer' 
 			onClick = {onClick}>{title} </motion.a>
 		</Link>
 	)
