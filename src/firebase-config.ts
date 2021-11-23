e063a8a79c3d806d62aa02194581be79e5ad1578
jdpena28 <johnhenrich1128@gmail.com>
2021-11-23 23:25:33 +0800

    Remove prefix NEXT_PUBLIC

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -2,12 +2,12 @@ import {initializeApp} from 'firebase/app';
 import {getFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_APIKEY,
-    authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
-    projectId: process.env.NEXT_PUBLIC_PROJECTID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGE_ID,
-    appId: process.env.NEXT_PUBLIC_APPID,
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGE_ID,
+    appId: process.env.APPID,
 };
 
 const app = initializeApp(firebaseConfig);
