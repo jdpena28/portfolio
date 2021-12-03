60ac25c5029792ab6819195a32b173ca17ef4794
jdpena28 <johnhenrich1128@gmail.com>
2021-12-03 15:45:53 +0800

    Function: when onClick choose to show what message

--- pages/message.tsx
+++ pages/message.tsx
@@ -6,28 +6,28 @@ import { auth } from '../src/firebase-config'
 const Message = () => {
     const [email,setEmail] = useState<string>('')
     const [password,setPassword] = useState<string>('')
+    const [openMessages,setOpenMessages] = useState<boolean>(false)
 
     const login = async (e:FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         await signInWithEmailAndPassword(auth,email, password)
-        .then((user) => {
-            alert('Login Successful')
+        .then(() => {
+            (e.target as HTMLFormElement).reset()
+            setOpenMessages(true)
         })
         .catch((error) => {
-            alert('Login Failed')
+            
         })
     }
 
-    console.log(email, password)
-
     return (
         <Layout>
-           <section className='w-full h-screen container mx-auto text-gray-200'>
-               <form 
+           <section className='w-full h-screen container mx-auto text-gray-100'>
+               {!openMessages&&<form 
                onSubmit={(e) => {login(e)}}
                className='flex flex-col gap-y-3 max-w-md  p-5 rounded-xl mx-auto mt-28 bg-gray-400'>
                    <label htmlFor="email">Email<br />
-                       <input className='text-black placeholder-gray-600 pl-1 w-full' 
+                       <input className='text-black placeholder-gray-400 pl-1 w-full' 
                        onChange={(e)=>setEmail(e.target.value)}
                        type="email" id='email' required placeholder='johndoe@gmail.com' />
                    </label>
@@ -37,7 +37,15 @@ const Message = () => {
                        type='password' id='password' required />
                    </label>
                    <button type='submit' className='bg-blue-500'>Login</button>
-               </form>
+               </form>}
+               {openMessages && <>
+               <button className='w-36 h-12 my-auto bg-purple-500 rounded-xl'>Log Out</button>
+               <div className='mx-auto h-full max-w-md flex justify-around'>
+                    <button className='h-12 w-36 my-auto bg-blue-500 rounded-xl'>Client Message</button>
+                    <button className='w-36 h-12 my-auto bg-pink-500 rounded-xl'>Birthday Message</button>
+               </div>
+               </>
+               }
            </section>
         </Layout>
     )
